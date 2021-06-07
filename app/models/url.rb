c28ca63c7e9600ef6ab1dbfd84f9d05103ada1c0
Yola21 <yashkhorja4@gmail.com>
2021-06-07 22:06:28 +0530

    Added presence for slug validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ApplicationRecord
   validates :url, presence: true, format: URI::regexp(%w[http https]), length: { maximum: 250 }
   validates :shortened_url, format: URI::regexp(%w[http https]), presence: true
-  validates :slug, uniqueness: true
+  validates :slug, uniqueness: true, presence: true
   enum status: { unpinned: 0, pinned: 1 }
 
   private
